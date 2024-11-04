2357a016a04952d1196d9741e4810c1b2ac1a042
Hugo Pernet <hugopernet@ssw.fr>
2024-11-04 15:49:53 +0100

    Adding CSP config to allow Tina's iframe to render in admin mode

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,17 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
+  headers: [
+    {
+      source: '*',
+      headers: [
+        {
+          key: 'Content-Security-Policy',
+          value: 'frame-ancestors *;',
+        },
+      ],
+    },
+  ],
 }
 
 export default config
